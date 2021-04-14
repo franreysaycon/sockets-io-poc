91a1200b35d5c105f0016251bfdaa3c7b5e8f22d
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-04-14 11:55:36 +0800

    Add strategies

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ const io = installSockets(server, {
 const NEW_ALERTS_EVENT = "new alerts"
 const ACK_ALERTS_EVENT = "ack alerts"
 
+const users = {}
+
 app.use(cors())
 app.use(express.json())
 
@@ -27,23 +29,46 @@ app.post('/new', (req, res) => {
     const id = v4()
     oldAlerts[v4()] = {
         data: req.body.data,
+        strategy: req.body.strategy,
         ack_person: null,
     }
 
-    io.emit(NEW_ALERTS_EVENT, { id: id, data: req.body.data })
+    console.log(users)
+    for (let [id, socket] of io.of("/").sockets) {
+        if (!!users[socket.handshake.auth.username].strategies.find(st => st === +req.body.strategy)) {
+            io.to(id).emit(NEW_ALERTS_EVENT, { id: id, data: req.body.data })
+        }
+    }
 
     fs.writeFileSync('data.json', JSON.stringify(oldAlerts))
     res.json({ success: true })
 })
 
 // Amps Websocket
+io.use((socket, next) => {
+    const username = socket.handshake.auth.username;
+    if (!username) {
+        return next(new Error("invalid username"));
+    }
+    socket.username = username;
+    next();
+});
+
 io.on('connection', (socket) => {
-    // Acknowledgement on sockets
+    if (!users[socket.handshake.auth.username]) {
+        users[socket.handshake.auth.username] = { strategies: [] }
+    }
+
     socket.on(ACK_ALERTS_EVENT, (data) => {
         const oldAlerts = JSON.parse(fs.readFileSync('data.json'))
         oldAlerts[data.id].ack_person = data.person
         socket.emit(ACK_ALERTS_EVENT, { id: data.id, ack_person: data.person })
         fs.writeFileSync('data.json', JSON.stringify(oldAlerts))
+    })
+
+    socket.on('strategies', (data) => {
+        users[socket.handshake.auth.username].strategies = data
+        console.log(users)
     })
 })
 
--- tester/src/App.js
+++ tester/src/App.js
@@ -1,30 +1,77 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { io } from 'socket.io-client'
 
+const socket = io("http://localhost:8000", { autoConnect: false });
+
 function App() {
+  const [username, setUsername] = useState(null)
   const [numbers, setNumbers] = useState([])
+  const [strategies, setStrategies] = useState([1, 2, 3])
+  const usernameRef = useRef()
+  const loggedInRef = useRef(false)
 
-  const socket = io("ws://localhost:8000")
+  useEffect(() => {
+    if (username && !loggedInRef.current) {
+      socket.auth = { username }
+      socket.connect()
+      socket.on('connect', () => {
+        socket.emit('strategies', strategies)
+      })
+      loggedInRef.current = true
+    }
+  }, [username, strategies])
 
-  socket.on("connect", () => {
-    console.log("CONNECTED!")
-  });
+  useEffect(() => {
+    socket.on("new alerts", data => {
+      console.log(data)
+      setNumbers([...numbers, data])
+    });
+  }, [numbers, setNumbers])
 
-  socket.on("new alerts", data => {
-    console.log(data)
-    setNumbers([...numbers, data])
-  });
+  const submitUsername = () => {
+    setUsername(usernameRef.current.value)
+    socket.emit('login', username)
+  }
+  console.log(strategies)
+  const toggleStrategy = (strat) => {
+    let newStrategies = strategies
+
+    if (!!strategies.find(st => st === strat)) {
+      newStrategies = strategies.filter(st => st !== strat)
+      setStrategies(newStrategies)
+    }
+    else {
+      newStrategies = [...strategies, strat]
+      setStrategies(newStrategies)
+    }
+
+    socket.emit('strategies', newStrategies)
+  }
 
   return (
-    <div>
+    <>
       {
-        numbers.map(num => (
-          <div key={num.id}>
-            {num.data}
+        !username ? <div>
+          <input type="text" ref={usernameRef} placeholder="who are you?" />
+          <button onClick={submitUsername}>Submit</button>
+        </div> :
+          <div>
+            <div style={{ display: "flex", flexDirection: 'column' }}>
+              <span><input type="checkbox" checked={!!strategies.find(st => st === 1)} onChange={() => toggleStrategy(1)} />Strategy 1</span>
+              <span><input type="checkbox" checked={!!strategies.find(st => st === 2)} onChange={() => toggleStrategy(2)} />Strategy 2</span>
+              <span><input type="checkbox" checked={!!strategies.find(st => st === 3)} onChange={() => toggleStrategy(3)} />Strategy 3</span>
+            </div>
+            {
+
+              numbers.map(num => (
+                <div key={num.id}>
+                  {num.data}
+                </div>
+              ))
+            }
           </div>
-        ))
       }
-    </div>
+    </>
   );
 }
 
